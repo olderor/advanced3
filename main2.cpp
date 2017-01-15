b0cc22aadcd465c983690de925e8038fea8e1e1b
Yevchenko Bohdan <98body@gmail.com>
2017-01-15 12:04:36 +0200

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -157,7 +157,7 @@ void write_data(
     std::vector<int> &data);
 
 // Main function.
-int main();
+const int main();
 
 
 treap::treap(const int size) {
@@ -341,14 +341,14 @@ void read_data(
 
 void write_data(
     std::ostream &_Ostr,
-    std::vector<int> &data) {
+    const std::vector<int> &data) {
 
     for (int i = 0; i < data.size(); ++i) {
         _Ostr << data[i] << " ";
     }
 }
 
-int main() {
+const int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
@@ -358,7 +358,7 @@ int main() {
 
     read_data(std::cin, size, queries_count, queries);
 
-    std::vector<int> result = solve(size, queries_count, queries);
+    const std::vector<int> result = solve(size, queries_count, queries);
 
     write_data(std::cout, result);
 
